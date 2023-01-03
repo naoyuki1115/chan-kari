6e576fd87266716a54d66bb528d681eae618ed1b
Boushipan <base.space.ff@gmail.com>
2023-01-03 14:31:51 +0900

    router set

--- frontend/docker-react/react/frontend/chan-kari/src/App.jsx
+++ frontend/docker-react/react/frontend/chan-kari/src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Fragment } from "react";
 import ReactDOM from "react-dom"
 import './App.css';
@@ -5,14 +6,33 @@ import './App.css';
 
 export const App = () => {
   return (
-    <Fragment>
-      <h1>チャンカリ</h1>
-      <p>ホームページ</p>
-    </Fragment>
+    <div className="App">
+      <div>
+        <h1>チャンカリ</h1>
+        <p>ホームページ</p>
+      </div>
 
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+
+
+//ReactDOM.render(<App />, document.getElementById("root"));
 
 
