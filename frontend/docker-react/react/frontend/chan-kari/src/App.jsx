44c56dde2e119d75cce86e9ee9c993a3709cdeb1
Boushipan <base.space.ff@gmail.com>
2023-01-08 15:02:44 +0900

    Add menu

--- frontend/docker-react/react/frontend/chan-kari/src/App.jsx
+++ frontend/docker-react/react/frontend/chan-kari/src/App.jsx
@@ -2,33 +2,52 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Fragment } from "react";
 import ReactDOM from "react-dom"
 import './App.css';
-
+import Home from './component/Home';
 
 export const App = () => {
   return (
     <div className="App">
       <div>
         <h1>チャンカリ</h1>
-        <p>ホームページ</p>
       </div>
 
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Routes>
+        <Route path="/item_register" element={<Item_register />} />
+      </Routes>
+      <Routes>
+        <Route path="/mypage" element={<Mypage />} />
+      </Routes>
+
+      <dev className="menu">
+        <h3>メニュー</h3>
+        <ul>
+          <li>
+            <a href="/">ホーム</a>
+          </li>
+          <li>
+            <a href="/item_register">登録</a>
+          </li>
+          <li>
+            <a href="/mypage">マイページ</a>
+          </li>
+        </ul>
+
+      </dev>
     </div>
   );
 };
 
-function Home() {
-  return <h2>Home</h2>;
+
+
+function Item_register() {
+  return <h2>貸出登録</h2>;
 }
 
-function About() {
-  return <h2>About</h2>;
-}
-
-function Contact() {
-  return <h2>Contact</h2>;
+function Mypage() {
+  return <h2>マイページ</h2>;
 }
 
 
