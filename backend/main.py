a37e48e783d5a2c5a28b3e7807892016d2cb41ec
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-01-28 21:01:35 +0900

    Add route of API

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
+from fastapi import FastAPI
+
 from database.database import engine
 from model import Base
+from router import item, rental, user
 
 # migrate table
 Base.metadata.create_all(engine)
+
+
+app = FastAPI()
+app.include_router(user.router)
+app.include_router(item.router)
+app.include_router(rental.router)
--- backend/router/user.py
+++ backend/router/user.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 router = APIRouter(
-    prefix="/items",
+    prefix="/users",
 )
 
 
