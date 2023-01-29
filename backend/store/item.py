84b9242a064b1075f858b98830a2d680bf927387
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-01-29 16:49:14 +0000

    Implement store to get item list

--- backend/store/item.py
+++ backend/store/item.py
@@ -1,10 +1,21 @@
 from typing import Optional
 
 import model
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 
 def list_available(
     db: Session, limit: int, after: Optional[int], before: Optional[int]
 ) -> list[model.Item]:
-    return []
+    q = (
+        db.query(model.Item)
+        .filter(model.Item.available is True)
+        .order_by(desc(model.Item.id))
+        .limit(limit)
+    )
+    if after is not None:
+        return q.offset(after).all()
+    elif before is not None:
+        return q.offset(before).all()
+    return q.all()
