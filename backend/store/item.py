e9d2ad44350aaac95cff647d3a202f1d221703f8
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-01-30 17:30:58 +0000

    Modify query

--- backend/store/item.py
+++ backend/store/item.py
@@ -8,18 +8,17 @@ from sqlalchemy.orm import Session
 def list_available(
     db: Session, limit: int, after: Optional[int], before: Optional[int]
 ) -> list[model.Item]:
-    q = (
-        db.query(model.Item)
-        .filter(model.Item.available == True)
-        .order_by(model.Item.id)
-    )
+    q = db.query(model.Item).filter(model.Item.available == True)
     if after is not None:
-        q = q.offset(after)
+        return q.order_by(model.Item.id).offset(after).limit(limit).all()
     elif before is not None:
-        if before > limit:
-            offset = before - limit
-        else:
-            offset = 0
-            limit = before - 1
-        q = q.offset(offset)
-    return q.limit(limit).all()
+        items = (
+            q.order_by(desc(model.Item.id))
+            .filter(model.Item.id < before)
+            .limit(limit)
+            .all()
+        )
+        items.reverse()
+        return items
+    else:
+        return q.order_by(model.Item.id).limit(limit).all()
