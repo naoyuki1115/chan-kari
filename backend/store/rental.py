198e9a91113a9a872d6985b609d95f95b3fce150
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-02-02 23:49:33 +0000

    Add flake8 error ingnore comment-out to SQL null check

--- backend/store/rental.py
+++ backend/store/rental.py
@@ -1,12 +1,11 @@
 import model
-from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 
 def list_not_returned(db: Session) -> list[model.Rental]:
     return (
         db.query(model.Rental)
-        .filter(model.Rental.returned_at == None)
+        .filter(model.Rental.returned_at == None)  # NOQA
         .order_by(model.Rental.id)
         .all()
     )
