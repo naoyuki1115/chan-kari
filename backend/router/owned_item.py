1e48377547c18f67b270ca4eb9f2ba03f775653f
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-02-04 13:37:18 +0000

    Implement router to create new item

---
+++ backend/router/owned_item.py
@@ -0,0 +1,36 @@
+from database.database import get_db
+from database.transaction import Transaction, TransactionInterface
+from fastapi import APIRouter, Depends, HTTPException, status
+from schema import ItemCreateRequest, ItemCreateResponse
+from sqlalchemy.orm import Session
+from store import ItemStore, ItemStoreInterface, RentalStore, RentalStoreInterface
+from usecase import ItemUseCase, ItemUseCaseInterface
+from util.logging import get_logger
+
+router = APIRouter(
+    prefix="/items",
+)
+
+
+def new_item_usecase(db: Session = Depends(get_db)) -> ItemUseCaseInterface:
+    tx: TransactionInterface = Transaction(db)
+    item_store: ItemStoreInterface = ItemStore(db)
+    renal_store: RentalStoreInterface = RentalStore(db)
+    return ItemUseCase(tx, item_store, renal_store)
+
+
+@router.post("", response_model=ItemCreateResponse)
+def create_item(
+    req: ItemCreateRequest,
+    item_usecase: ItemUseCaseInterface = Depends(new_item_usecase),
+) -> ItemCreateResponse:
+    try:
+        # TODO: headerのトークンからユーザーID取得
+        test_user_id = 1
+        item = item_usecase.create_item(req, test_user_id)
+    except Exception as e:
+        get_logger(__name__).error(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+    return item
