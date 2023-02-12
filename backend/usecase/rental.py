54188c03b8c30162a53ba69e729cb5ebcfa2010b
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-02-12 13:03:50 +0000

    Implement usecase to return rental item

--- backend/usecase/rental.py
+++ backend/usecase/rental.py
@@ -1,4 +1,6 @@
 import abc
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 import model
 from database.transaction import TransactionInterface
@@ -69,4 +71,34 @@ class RentalUseCase(RentalUseCaseInterface):
             raise
 
     def return_item(self, params: ReturnParams, user_id: int) -> None:
-        raise NotImplementedError
+        try:
+            existing_rental = self.rental_store.detail(params.rental_id)
+            if existing_rental is None:
+                raise NotFoundError
+            if existing_rental.user_id != user_id:
+                raise OperationIsForbiddenError
+            if existing_rental.returned_date is not None:
+                raise ResourceAlreadyExistsError
+
+            rental = model.Rental(
+                id=existing_rental.id,
+                user_id=existing_rental.user_id,
+                item_id=existing_rental.item_id,
+                rented_date=existing_rental.rented_date,
+                return_plan_date=existing_rental.return_plan_date,
+                returned_date=datetime.now(ZoneInfo("Asia/Tokyo")).date(),
+            )
+            self.rental_store.update(rental)
+            self.tx.commit()
+        except UniqueViolation as err:
+            logger.error(f"({__name__}): {err}")
+            self.tx.rollback()
+            raise ResourceAlreadyExistsError
+        except ForeignKeyViolation as err:
+            logger.error(f"({__name__}): {err}")
+            self.tx.rollback()
+            raise NotFoundError
+        except Exception as err:
+            logger.error(f"({__name__}): {err}")
+            self.tx.rollback()
+            raise
