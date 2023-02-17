f872c22cae0b6722cddf9451ed03678231341fc5
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-02-17 04:53:06 +0000

    fastapi check

--- backend/router/route.py
+++ backend/router/route.py
@@ -3,6 +3,12 @@ from router import item, owned_item, rental, user
 
 router = APIRouter()
 
+
+@router.get("/test")
+def list_item() -> None:
+    print("test")
+
+
 router.include_router(user.router, prefix="/users", tags=["users"])
 router.include_router(item.router, prefix="/items", tags=["items"])
 router.include_router(owned_item.router, prefix="/users/items", tags=["owned_items"])
