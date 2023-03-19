db4f7b9569a2c9e6a10dad5b143d597a225973bc
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-03-19 20:15:00 +0900

    Delete unneccesary code in usecase

--- backend/usecase/item.py
+++ backend/usecase/item.py
@@ -1,18 +1,10 @@
 import abc
 
 import domain_model
-import model
 from database.transaction import TransactionInterface
 from psycopg2.errors import ForeignKeyViolation
 from repository import ItemStoreInterface, RentalStoreInterface
-from schema import (
-    ItemCreateRequest,
-    ItemCreateResponse,
-    ItemListParams,
-    ItemResponse,
-    ItemStatus,
-    PaginationQuery,
-)
+from schema import ItemCreateRequest, ItemListParams, PaginationQuery
 from util.error_msg import NotFoundError
 from util.logging import get_logger
 
@@ -23,17 +15,17 @@ class ItemUseCaseInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def get_list(
         self, pagination: PaginationQuery, params: ItemListParams
-    ) -> list[ItemResponse]:
+    ) -> list[domain_model.Item]:
         raise NotImplementedError
 
     @abc.abstractmethod
     def get_my_list(
         self, pagination: PaginationQuery, user_id: int
-    ) -> list[ItemResponse]:
+    ) -> list[domain_model.Item]:
         raise NotImplementedError
 
     @abc.abstractmethod
-    def create_item(self, req: ItemCreateRequest, user_id: int) -> ItemCreateResponse:
+    def create_item(self, req: ItemCreateRequest, user_id: int) -> domain_model.Item:
         raise NotImplementedError
 
 
@@ -50,36 +42,6 @@ class ItemUseCase(ItemUseCaseInterface):
 
     def get_list(
         self, pagination: PaginationQuery, params: ItemListParams
-    ) -> list[ItemResponse]:
-        try:
-            items: list[model.Item] = self.item_store.list_available(pagination)
-            valid_rentals: list[model.Rental] = self.rental_store.list_valid()
-
-            item_res_list: list[ItemResponse] = []
-            for item in items:
-                if not item.available:
-                    continue
-                elif (
-                    len(list(filter(lambda r: r.item_id == item.id, valid_rentals)))
-                    != 0
-                ):
-                    status: ItemStatus = ItemStatus.rented
-                else:
-                    status: ItemStatus = ItemStatus.available
-                item_res_list.append(
-                    ItemResponse.new(item.id, item.name, status, item.image_url)
-                )
-            if bool(params.available):
-                return list(
-                    filter(lambda i: i.status == ItemStatus.available, item_res_list)
-                )
-            return item_res_list
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            raise
-
-    def get_list2(
-        self, pagination: PaginationQuery, params: ItemListParams
     ) -> list[domain_model.Item]:
         try:
             return self.item_store.list_public(pagination, bool(params.available))
@@ -89,64 +51,14 @@ class ItemUseCase(ItemUseCaseInterface):
 
     def get_my_list(
         self, pagination: PaginationQuery, user_id: int
-    ) -> list[ItemResponse]:
-        try:
-            items: list[model.Item] = self.item_store.list_by_user_id(
-                pagination, user_id
-            )
-            valid_rentals: list[model.Rental] = self.rental_store.list_valid()
-
-            item_res_list: list[ItemResponse] = []
-            for item in items:
-                if not item.available:
-                    status: ItemStatus = ItemStatus.unavailable
-                elif (
-                    len(list(filter(lambda r: r.item_id == item.id, valid_rentals)))
-                    != 0
-                ):
-                    status: ItemStatus = ItemStatus.rented
-                else:
-                    status: ItemStatus = ItemStatus.available
-                item_res_list.append(
-                    ItemResponse.new(item.id, item.name, status, item.image_url)
-                )
-            return item_res_list
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            raise
-
-    def get_my_list2(
-        self, pagination: PaginationQuery, user_id: int
     ) -> list[domain_model.Item]:
         try:
-            return self.item_store.list_by_user_id2(pagination, user_id)
+            return self.item_store.list_by_user_id(pagination, user_id)
         except Exception as err:
             logger.error(f"({__name__}): {err}")
             raise
 
-    def create_item(self, req: ItemCreateRequest, user_id: int) -> ItemCreateResponse:
-        try:
-            item = model.Item(
-                name=req.name,
-                owner_id=user_id,
-                available=not req.draft,
-                image_url=req.image_url,
-                description=req.description,
-                author=req.author,
-            )
-            self.item_store.create(item)
-            self.tx.commit()
-            return ItemCreateResponse.new(item.id)
-        except ForeignKeyViolation as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise NotFoundError
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise
-
-    def create_item2(self, req: ItemCreateRequest, user_id: int) -> domain_model.Item:
+    def create_item(self, req: ItemCreateRequest, user_id: int) -> domain_model.Item:
         try:
             item: domain_model.Item = domain_model.Item(
                 req.name, user_id, req.image_url, req.description, req.author
@@ -155,7 +67,7 @@ class ItemUseCase(ItemUseCaseInterface):
                 item.set_private_status()
             else:
                 item.set_public_status()
-            self.item_store.create2(item)
+            self.item_store.create(item)
             self.tx.commit()
             return item
         except ForeignKeyViolation as err:
--- backend/usecase/rental.py
+++ backend/usecase/rental.py
@@ -1,27 +1,12 @@
 import abc
-from datetime import datetime
 from typing import Optional
-from zoneinfo import ZoneInfo
 
 import domain_model
-import model
 from database.transaction import TransactionInterface
 from psycopg2.errors import ForeignKeyViolation, UniqueViolation
 from repository import ItemStoreInterface, RentalStoreInterface
-from schema import (
-    PaginationQuery,
-    RentalListParams,
-    RentalResponse,
-    RentRequest,
-    RentResponse,
-    ReturnParams,
-)
-from util.error_msg import (
-    NotFoundError,
-    OperationIsForbiddenError,
-    ResourceAlreadyExistsError,
-    ResourceUnavailableError,
-)
+from schema import PaginationQuery, RentalListParams, RentRequest, ReturnParams
+from util.error_msg import NotFoundError, ResourceAlreadyExistsError
 from util.logging import get_logger
 
 logger = get_logger()
@@ -29,7 +14,7 @@ logger = get_logger()
 
 class RentalUseCaseInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def rent_item(self, req: RentRequest, user_id: int) -> RentResponse:
+    def rent_item(self, req: RentRequest, user_id: int) -> domain_model.Rental:
         raise NotImplementedError
 
     @abc.abstractmethod
@@ -39,7 +24,7 @@ class RentalUseCaseInterface(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def get_my_list(
         self, pagination: PaginationQuery, params: RentalListParams, user_id: int
-    ) -> list[RentalResponse]:
+    ) -> list[domain_model.Rental]:
         raise NotImplementedError
 
 
@@ -54,65 +39,23 @@ class RentalUseCase(RentalUseCaseInterface):
         self.item_store: ItemStoreInterface = item
         self.rental_store: RentalStoreInterface = rental
 
-    def rent_item(self, req: RentRequest, user_id: int) -> RentResponse:
-        try:
-            item: model.Item = self.item_store.detail(req.item_id)
-            if item is None:
-                raise NotFoundError
-            if item.owner_id == user_id:
-                raise OperationIsForbiddenError
-            if item.available is False:
-                raise ResourceUnavailableError
-
-            rental = model.Rental(
-                user_id=user_id,
-                item_id=req.item_id,
-                rented_date=req.rental_date,
-                return_plan_date=req.return_plan_date,
-            )
-            self.rental_store.create(rental)
-            self.tx.commit()
-            return RentResponse.new(rental.id)
-        except UniqueViolation as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise ResourceAlreadyExistsError
-        except ForeignKeyViolation as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise NotFoundError
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise
-
-    def rent_item2(self, req: RentRequest, user_id: int) -> domain_model.Rental:
-        item = self.item_store.detail2(req.item_id)
+    def rent_item(self, req: RentRequest, user_id: int) -> domain_model.Rental:
+        item = self.item_store.detail(req.item_id)
         rental = domain_model.Rental(
             user_id, item, req.rental_date, req.return_plan_date
         )
-        self.rental_store.create2(rental)
+        self.rental_store.create(rental)
         self.tx.commit()
         return rental
 
     def return_item(self, params: ReturnParams, user_id: int) -> None:
         try:
-            existing_rental: model.Rental = self.rental_store.detail(params.rental_id)
-            if existing_rental is None:
-                raise NotFoundError
-            if existing_rental.user_id != user_id:
-                raise OperationIsForbiddenError
-            if existing_rental.returned_date is not None:
-                raise ResourceAlreadyExistsError
-
-            rental = model.Rental(
-                id=existing_rental.id,
-                user_id=existing_rental.user_id,
-                item_id=existing_rental.item_id,
-                rented_date=existing_rental.rented_date,
-                return_plan_date=existing_rental.return_plan_date,
-                returned_date=datetime.now(ZoneInfo("Asia/Tokyo")).date(),
+            rental: Optional[domain_model.Rental] = self.rental_store.detail(
+                params.rental_id
             )
+            if rental is None:
+                raise NotFoundError
+            rental.return_item(user_id)
             self.rental_store.update(rental)
             self.tx.commit()
         except UniqueViolation as err:
@@ -128,59 +71,11 @@ class RentalUseCase(RentalUseCaseInterface):
             self.tx.rollback()
             raise
 
-    def return_item2(self, params: ReturnParams, user_id: int) -> None:
-        try:
-            rental: Optional[domain_model.Rental] = self.rental_store.detail2(
-                params.rental_id
-            )
-            if rental is None:
-                raise NotFoundError
-            rental.return_item(user_id)
-            self.rental_store.update2(rental)
-            self.tx.commit()
-        except UniqueViolation as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise ResourceAlreadyExistsError
-        except ForeignKeyViolation as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise NotFoundError
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            self.tx.rollback()
-            raise
-
     def get_my_list(
-        self, pagination: PaginationQuery, params: RentalListParams, user_id: int
-    ) -> list[RentalResponse]:
-        try:
-            rentals: list[model.Rental] = self.rental_store.list_by_user_id(
-                user_id, bool(params.closed), pagination
-            )
-            rental_res_list: list[RentalResponse] = []
-            for rental in rentals:
-                rental_res_list.append(
-                    RentalResponse.new(
-                        id=rental.id,
-                        closed=False if rental.returned_date is None else True,
-                        rental_date=rental.rented_date,
-                        return_plan_date=rental.return_plan_date,
-                        return_date=rental.returned_date,
-                        item_id=rental.item_id,
-                        item_name=rental.items.name,
-                    )
-                )
-            return rental_res_list
-        except Exception as err:
-            logger.error(f"({__name__}): {err}")
-            raise
-
-    def get_my_list2(
         self, pagination: PaginationQuery, params: RentalListParams, user_id: int
     ) -> list[domain_model.Rental]:
         try:
-            return self.rental_store.list_by_user_id2(
+            return self.rental_store.list_by_user_id(
                 user_id, bool(params.closed), pagination
             )
         except Exception as err:
