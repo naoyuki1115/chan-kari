660423cda0df5e3d739bd9c354fe4e30c622bf88
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-03-21 18:15:26 +0900

    Add CORS setting (add allow origin)

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,23 @@
+import os
 from database.database import create_table
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import PlainTextResponse
 from router import route
+from fastapi.middleware.cors import CORSMiddleware
 
 create_table()
 
 app = FastAPI()
 app.include_router(route.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.environ.get("FRONTEND_ORIGIN")],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(RequestValidationError)
