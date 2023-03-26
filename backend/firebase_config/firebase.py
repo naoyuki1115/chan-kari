d63ff36692705bc8960435f33b64f7568b90721f
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-03-26 17:22:01 +0900

    Fix firebase config

--- backend/firebase_config/firebase.py
+++ backend/firebase_config/firebase.py
@@ -1,3 +1,7 @@
-import firebase_admin
+import os
 
-firebase_app = firebase_admin.initialize_app()
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate(os.environ.get("FIREBASE_CREDENTIALS_PATH"))
+firebase_app = firebase_admin.initialize_app(cred)
