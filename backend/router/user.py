82887a184afd971c7e27ba2e42aa852919b3887e
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-03-26 16:14:46 +0900

    Add API endpoint for signup

--- backend/router/user.py
+++ backend/router/user.py
@@ -1,6 +1,53 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, HTTPException, status
+from firebase_admin.auth import UserRecord
+from sqlalchemy.orm import Session
 
+from auth.auth import authenticate_user
+from database.database import get_db
+from database.transaction import Transaction, TransactionInterface
+from repository import UserStoreInterface
+from store import UserStore
+from usecase import UserUseCase, UserUseCaseInterface
+from util.error_msg import UnauthorizedUserError
+from util.logging import get_logger
+
+logger = get_logger()
 router = APIRouter()
+
+
+def new_user_usecase(db: Session = Depends(get_db)) -> UserUseCaseInterface:
+    tx: TransactionInterface = Transaction(db)
+    user_store: UserStoreInterface = UserStore(db)
+    return UserUseCase(tx, user_store)
+
+
+@router.post("/signup", status_code=status.HTTP_204_NO_CONTENT)
+def register_user(
+    user_usecase: UserUseCaseInterface = Depends(new_user_usecase),
+    idp_user: UserRecord = Depends(authenticate_user),
+) -> None:
+    try:
+        if (
+            idp_user.uid is None
+            or idp_user.display_name is None
+            or idp_user.email is None
+        ):
+            raise UnauthorizedUserError
+        user_usecase.create_user(
+            uid=idp_user.uid,
+            name=idp_user.display_name,
+            email=idp_user.email,
+            image_url=idp_user.photo_url,
+        )
+    except UnauthorizedUserError as err:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=err.message
+        )
+    except Exception as err:
+        logger.error(f"({__name__}): {err}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
 
 
 @router.get("/profile")
