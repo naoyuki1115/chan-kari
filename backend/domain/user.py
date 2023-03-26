db763d7a83b43a26cf19447bf91910b8cdf12f6f
naoyuki1115 <s.nao0o0o15@gmail.com>
2023-03-26 11:53:29 +0900

    Add uid to User model

--- backend/domain/user.py
+++ backend/domain/user.py
@@ -2,17 +2,22 @@ from typing import Optional
 
 
 class User:
-    __id: int
+    __id: Optional[int]
+    __uid: str
     __name: str
     __email: str
     __image_url: Optional[str] = None
 
     def __init__(
         self,
+        uid: str,
         name: str,
         email: str,
         image_url: Optional[str] = None,
+        id: Optional[int] = None,
     ):
+        self.__id = id
+        self.__uid = uid
         self.__name = name
         self.__email: str = email
         self.__image_url: Optional[str] = image_url
--- backend/model/user.py
+++ backend/model/user.py
@@ -3,14 +3,26 @@ from sqlalchemy.orm import relationship
 
 from model import Base
 from model.timestamp import Timestamp
+from domain import User
 
 
 class UserDTO(Base, Timestamp):
     __tablename__ = "users"
     id: int = Column(Integer, primary_key=True, autoincrement=True)  # type: ignore
+    uid: str = Column(String, nullable=False, unique=True)  # type: ignore
     name: str = Column(String, nullable=False)  # type: ignore
     email: str = Column(String, nullable=False, unique=True)  # type: ignore
     image_url: str = Column(String, nullable=True)  # type: ignore
 
     items = relationship("ItemDTO", back_populates="owner")
     rentals = relationship("RentalDTO", back_populates="user")
+
+    def to_domain_model(self) -> User:
+        user = User(
+            uid=self.uid,
+            name=self.name,
+            email=self.email,
+            image_url=self.image_url,
+            id=self.id,
+        )
+        return user
